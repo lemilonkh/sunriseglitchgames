c8e78504fde4a81eee1d7f001b2b9f2421ea822b
Milan Gruner <milangruner@gmail.com>
2024-02-24 10:54:33 +0100

    Add simple-icons to astro-icon
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+  integrations: [
+    mdx(),
+    icon({
+      include: { mdi: ['*'], 'simple-icons': ['*'] },
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
 })
